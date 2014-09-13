19cba6da9a6245c248cdb36dfbec35edd457cf9f
Alois Reitbauer <alois@reitbauer.cc>
2014-09-13 17:11:11 -0400

    remove non needed example code

--- examples/xhrMeasure.js
+++
@@ -1,7 +0,0 @@
-
-
-
-var perfEntries = performance.getEntriesByType("measure");
-for(i = 0; i < perfEntries.length; i++){
-  console.log(perfEntries[i].name + ' took ' + perfEntries[i].duration.toFixed(2) + ' ms');
-}
