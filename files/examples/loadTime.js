8bf2369a9643d4056a8e47dd224c30ddc1edd386
Alois Reitbauer <alois@reitbauer.cc>
2014-09-13 09:10:36 -0400

    removed JS example file

--- files/examples/loadTime.js
+++
@@ -1,7 +0,0 @@
-// how long does it take to load a page?
-
-var timing= performance.timing;
-var loadTime= (timing.loadEventEnd- timing.navigationStart)/1000;
-console.log('Page took ' + loadTime.toFixed(2) + ' seconds to load');
-
-
