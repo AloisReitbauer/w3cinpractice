eb29abb04f51b28badc4021e1387da3e82e57200
Alois Reitbauer <alois@reitbauer.cc>
2014-09-14 12:43:42 -0400

    updated to also support the phases of navigation timing

--- files/housekeeping.js
+++ files/housekeeping.js
@@ -1,7 +1,23 @@
+// update navigation timing with additional calculated timings
 
+performance.timing.phase = new Object();
+if (performance.timing.secureConnectionStart == 0) {
+  performance.timing.phase.ssl = 0;
+} else {
+  performance.timing.phase.ssl = performance.timing.connectEnd - performance.timing.secureConnectionStart;
+}
+performance.timing.phase.tcp = performance.timing.connectEnd - performance.timing.connectStart;
+performance.timing.phase.dns = performance.timing.domainLookupEnd - performance.timing.domainLookupStart;
+performance.timing.phase.redirect = performance.timing.redirectEnd - performance.timing.redirectStart;
+performance.timing.phase.appCache = performance.timing.domainLookupStart - performance.timing.fetchStart;
+performance.timing.phase.request = performance.timing.responseStart - performance.timing.requestStart;
+performance.timing.phase.response = performance.timing.responseEnd - performance.timing.responseStart;
+performance.timing.phase.load = performance.timing.loadEventEnd - performance.timing.loadEventStart;
+performance.timing.phase.unload = performance.timing.domainLookEnd - performance.timing.domainLookStart;
+performance.timing.phase.DOMContentLoaded = performance.timing.domContentLoadedEventEnd - performance.timing.domContentLoadedEventStart;
 
+// update performance entries with additional calculated timings
 var perfEntries = performance.getEntries();
-
 for (var i=0; i < perfEntries.length; i++){
   entry = perfEntries[i];
   entry.timing = new Object();
@@ -16,8 +32,8 @@ for (var i=0; i < perfEntries.length; i++){
   }
 }
 
+// add fetch by initiator to performance interface
 window.performance.getEntriesbyInitiator = function (initiator){
-
   var entries = new Array ();
   for(var i= 0; i < perfEntries.length; i++){
     if(perfEntries[i].initiatorType == initiator){
